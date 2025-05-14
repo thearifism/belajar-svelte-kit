d92bbff980c5a3e1e9f194f9665defef110b626f
krearive <arifblogger77@gmail.com>
2025-05-14 11:02:41 +0700

    Add navigation page with input and redirect functionality
    Introduce a new navigation page that allows users to input a location and navigate to it using the `goto` function from SvelteKit. This enables dynamic redirection based on user input.

---
+++ src/routes/navigation/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { goto } from "$app/navigation";
+
+  let location = $state("");
+
+  function go() {
+    goto(location);
+  }
+</script>
+
+<div>
+  <label for="location">Location</label>
+  <input type="text" id="location" bind:value={location} />
+  <button onclick={go}>Go</button>
+</div>
