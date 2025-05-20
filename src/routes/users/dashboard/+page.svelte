a444f609ef33a0829d564f51e30d41db161f51b9
krearive <arifblogger77@gmail.com>
2025-05-20 18:17:21 +0700

    Refactor user dashboard loading and add error page
    Moved user data fetching from client-side to server-side for better consistency and error handling. Implemented a new error page to display unauthorized access messages and redirect users to the login page.

--- src/routes/users/dashboard/+page.svelte
+++ src/routes/users/dashboard/+page.svelte
@@ -3,15 +3,6 @@
 
   const { data } = $props();
 
-  async function current() {
-    const response = await fetch("/api/users/current");
-    if (response.ok) {
-      return await response.json();
-    } else {
-      await goto("/users/login");
-    }
-  }
-
   async function logout() {
     const response = await fetch("/api/users/current", {
       method: "DELETE",
@@ -20,9 +11,5 @@
   }
 </script>
 
-{#await current()}
-  Loading user
-{:then data}
-  <h1>Hello {data.user}</h1>
-  <a href="#" onclick={logout}>Logout</a>
-{/await}
+<h1>Hello {data.user}</h1>
+<a href="#" onclick={logout}>Logout</a>
