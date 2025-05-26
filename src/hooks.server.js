d4aaaad350e36cc795be39ff615ef50ec0bf396b
krearive <arifblogger77@gmail.com>
2025-05-26 16:28:51 +0700

    Add environment variable imports and logging in init function
    Imported static and dynamic environment variables for both private and public scopes. Added console logs to verify environment values during application initialization.

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,3 +1,8 @@
+import { env } from '$env/dynamic/private';
+import { env as publicEnv } from '$env/dynamic/public';
+import { DB_HOST, DB_PORT } from '$env/static/private';
+import { PUBLIC_NAME } from '$env/static/public';
+
 export async function handle({ event, resolve }) {
   console.log(`receive request :  ${event.url.href}`);
 
@@ -6,4 +11,10 @@ export async function handle({ event, resolve }) {
 
 export function init() {
   console.log('init application');
+  console.log(DB_HOST);
+  console.log(DB_PORT);
+  console.log(PUBLIC_NAME);
+
+  console.log(env.HELLO);
+  console.log(publicEnv.PUBLIC_HELLO);
 }
