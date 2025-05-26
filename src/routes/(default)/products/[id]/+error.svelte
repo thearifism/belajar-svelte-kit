48f7fe664826527d73622a6538f2e2a3ade7bb00
krearive <arifblogger77@gmail.com>
2025-05-26 16:03:40 +0700

    Add error handling for product details page
    Introduce an error page to display status and message when a product is not found. Update the product details loader to throw a 404 error if the API response is unsuccessful.

---
+++ src/routes/(default)/products/[id]/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { page } from "$app/state";
+</script>
+
+<h1>Unexpected Error : {page.status}</h1>
+
+<p>Message : {page.error.message}</p>
--- src/routes/(default)/products/[id]/+page.js
+++ src/routes/(default)/products/[id]/+page.js
@@ -1,4 +1,13 @@
+import { error } from '@sveltejs/kit';
+
 export async function load({ params, fetch }) {
   const response = await fetch(`/api/products/${params.id}.json`);
+
+  if (response.status !== 200) {
+    error(404, {
+      message: "Product not found",
+    })
+  }
+
   return response.json();
 }
